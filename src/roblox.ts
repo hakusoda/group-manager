d92167c54db24878666afebca6305c055e3f12c0
katsumi143 <32640219+katsumi143@users.noreply.github.com>
2023-07-12 22:33:04 +1000

    fix authentication

--- src/roblox.ts
+++ src/roblox.ts
@@ -1,18 +1,27 @@
 import { ROBLOX_TOKEN } from './constants.ts';
 import type { HttpMethod } from './types.ts';
-export function makeRequest<T = any>(path: string, method: HttpMethod = 'GET', body?: any): Promise<{ success: false } | { data: T, success: true }> {
+export function makeRequest<T = any>(path: string, method: HttpMethod = 'GET', body?: any, headers?: Record<string, string>): Promise<{ success: false } | { data: T, success: true }> {
 	const options = {
 		body: body ? JSON.stringify(body) : undefined,
 		method,
 		headers: {
-			cookie: `.ROBLOSECURITY: ${ROBLOX_TOKEN}`,
-			'content-type': 'application/json'
+			cookie: `.ROBLOSECURITY=${ROBLOX_TOKEN};`,
+			'content-type': 'application/json',
+			...headers
 		}
 	} satisfies RequestInit;
 	return fetch(path, options)
 		.then(async response => {
 			if (response.status === 200)
 				return { data: await response.json(), success: true };
+			
+			const csrf = response.headers.get('x-csrf-token');
+			if (csrf)
+				return makeRequest(path, method, body, {
+					...headers,
+					'x-csrf-token': csrf
+				});
+
 			console.error(response.status, await response.text().catch(() => ''));
 			return { success: false };
 		});
